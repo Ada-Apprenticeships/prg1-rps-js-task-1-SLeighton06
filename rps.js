a66ab67ad36f3798e3200276d3b4a372287c2636
SLeighton06 <seth.leighton@ada.ac.uk>
2024-09-25 12:00:37 +0000

    Code refactored, ternary operator added to reduce lines

--- rps.js
+++ rps.js
@@ -1,23 +1,19 @@
 function rockPaperScissors(player1, player2) {
-  const WinningChoice = {
+  const winAgainst = {
     rock: ["scissors", "lizard"],
     paper: ["rock", "spock"],
     scissors: ["paper", "lizard"],
     lizard: ["paper", "spock"],
-    spock: ["rock", "scissors"],
-  }
+    spock: ["rock", "scissors"]
+  };
 
-  if (player1 === player2) {
-    return "draw";
-  } else if (WinningChoice[player1].includes(player2)) {
+  if (winAgainst[player1].includes(player2)) {
     return "player1";
-  } else {
-    return "player2";
-  }
-}
-
+  };
+  return player1 === player2 ? "draw" : "player2";
+};
 
 // Leave this code here for the automated tests
 module.exports = {
   rockPaperScissors,
-}
+};
